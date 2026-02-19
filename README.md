9a3dace57a01b24cb8872100a61e4860bebc3c10
SimonMaracine <simonmara.dev@gmail.com>
2026-02-19 15:33:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# static-allocator
+
+A global static stateless allocator. Header only. It is well suited for frequently allocated objects that have similar
+known sizes and when it is known an upper limit of the maximum amount of objects allocated at once.
+
+Simply copy the header file and integrate it into your build system however you want. Note the license.
+
+```cpp
+#include <list>
+
+#include "allocator.hpp"
+
+using MyObject1Storage = allocator::StaticAllocatorStorage<32, 8, 4>;
+using MyObject2Storage = allocator::StaticAllocatorStorage<64, 24, 8>;
+
+struct MyObject1 : allocator::StaticAllocated<MyObject1, MyObject1Storage> {
+    int a, b;
+};
+
+struct MyObject2 {
+    int a, b;
+};
+
+int main() {
+    MyObject1* obj1 {new MyObject1};
+    delete obj1;
+
+    std::list<MyObject2, allocator::StaticAllocator<MyObject2, MyObject2Storage>> obj2;
+}
+```
---
+++ allocator.hpp
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <memory>
+#include <algorithm>
+#include <utility>
+#include <new>
+#include <cstddef>
+
+namespace allocator {
+    template<std::size_t StorageSize, std::size_t BlockSize, std::size_t BlockAlignment>
+    struct StaticAllocatorStorage {
+    private:
+        static consteval std::size_t calculate_block_size() {
+            const auto quot {BlockSize / BlockAlignment};  // TODO __cpp_lib_constexpr_cmath is still undefined
+            const auto rem {BlockSize % BlockAlignment};
+
+            if (rem == 0) {
+                return BlockSize;
+            }
+
+            return (quot + 1) * BlockAlignment;
+        }
+    public:
+        static constexpr auto STORAGE_SIZE {StorageSize};
+        static constexpr auto BLOCK_SIZE {calculate_block_size()};
+        static constexpr auto BLOCK_ALIGNMENT {BlockAlignment};
+
+        alignas(BLOCK_ALIGNMENT) unsigned char m_base[STORAGE_SIZE * BLOCK_SIZE] {};
+        bool m_blocks[STORAGE_SIZE] {};
+        std::size_t m_pointer {};
+
+        static StaticAllocatorStorage& get() {
+            static StaticAllocatorStorage instance;
+            return instance;
+        }
+    };
+
+    template<typename T, typename Storage>
+    struct StaticAllocator {
+        using value_type = T;
+        using size_type = std::size_t;
+
+        static_assert(sizeof(T) <= Storage::BLOCK_SIZE);
+        static_assert(alignof(T) <= Storage::BLOCK_ALIGNMENT);
+
+        value_type* allocate(size_type n) {
+            auto& storage {Storage::get()};
+
+            for (size_type i {storage.m_pointer}; i < storage.STORAGE_SIZE - n + 1; i++) {
+                if (try_allocate(storage, i, n)) {
+                    return reinterpret_cast<value_type*>(storage.m_base + Storage::BLOCK_SIZE * i);
+                }
+            }
+
+            for (size_type i {}; i < storage.m_pointer - n + 1; i++) {
+                if (try_allocate(storage, i, n)) {
+                    return reinterpret_cast<value_type*>(storage.m_base + Storage::BLOCK_SIZE * i);
+                }
+            }
+
+#ifdef ALLOCATOR_THROW_ON_FAILURE
+            throw std::bad_alloc();
+#else
+            std::unreachable();
+#endif
+        }
+
+        void deallocate(value_type* p, size_type n) {
+            auto& storage {Storage::get()};
+
+            const size_type block_pointer {reinterpret_cast<size_type>(p) - reinterpret_cast<size_type>(storage.m_base)};
+            const size_type index {block_pointer / Storage::BLOCK_SIZE};
+
+            std::for_each(storage.m_blocks + index, storage.m_blocks + index + n, [](bool& block) { block = false; });
+        }
+    private:
+        static bool try_allocate(Storage& storage, size_type i, size_type n) {
+            if (std::all_of(storage.m_blocks + i, storage.m_blocks + i + n, [](const bool& block) { return !block; })) {
+                std::for_each(storage.m_blocks + i, storage.m_blocks + i + n, [](bool& block) { block = true; });
+                storage.m_pointer = i + 1;
+
+                return true;
+            }
+
+            return false;
+        }
+    };
+
+    template<typename T, typename U, typename Storage>
+    bool operator==(const StaticAllocator<T, Storage>&, const StaticAllocator<U, Storage>&) { return true; }
+
+    template<typename T, typename U, typename Storage>
+    bool operator!=(const StaticAllocator<T, Storage>&, const StaticAllocator<U, Storage>&) { return false; }
+
+    template<typename T, typename Storage>
+    struct StaticAllocated {
+        void* operator new(std::size_t) {
+            StaticAllocator<T, Storage> alloc;
+            using Alloc = std::allocator_traits<decltype(alloc)>;
+
+            return Alloc::allocate(alloc, 1);
+        }
+
+        void operator delete(void* ptr) {
+            StaticAllocator<T, Storage> alloc;
+            using Alloc = std::allocator_traits<decltype(alloc)>;
+
+            Alloc::deallocate(alloc, static_cast<T*>(ptr), 1);
+        }
+    };
+}
